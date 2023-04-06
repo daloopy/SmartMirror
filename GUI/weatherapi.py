b1196357131d98e4d83e4f169d60ea2317ba9af3
daloopy <dvaquera@purdue.edu>
2023-04-06 15:50:17 -0400

    changed quotes to single

--- GUI/weatherapi.py
+++ GUI/weatherapi.py
@@ -7,9 +7,9 @@ def getWeather(zipcode="47906"):
     url = "http://api.weatherapi.com/v1/current.json?key={}&q={}".format(token, zipcode)
     response = requests.get(url)
     data = json.loads(response.text)
-    town = data["location"]["name"]
-    temp_f = data["current"]["temp_f"]
-    condition = data["current"]["condition"]["text"]
+    town = data['location']['name']
+    temp_f = data['current']['temp_f']
+    condition = data['current']['condition']['text']
     return town, temp_f, condition
 
 town, temp, condition = getWeather("47906")
