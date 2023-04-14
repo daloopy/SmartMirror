659665400dbe877dd05ac7487e969d32d2aaa705
Dalilah Vaquera <70181295+daloopy@users.noreply.github.com>
2023-04-14 16:07:37 -0400

    digit keyboard works
    everything looks uggly but it works

--- GUI/keyboard.py
+++ GUI/keyboard.py
@@ -10,7 +10,6 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
 
 
-
 class VirtualKeyboard(GridLayout):
     def __init__(self, return_func=None, **kwargs):
         super().__init__(**kwargs)
@@ -23,23 +22,20 @@ class VirtualKeyboard(GridLayout):
         self.add_widget(self.virtual_keyboard, index = 1)
         
         # create label that will be updated
-        self.input = Label(text='Enter new zipcode: ')
+        self.input = Label(text='Enter input: ')
         self.add_widget(self.input, index = 0)
-        # Add the VirtualKeyboard widget to the content of the popup
-        # Create a label to display the input        
+      
         
     def key_up(self, keyboard, keycode, label=None, *args):
-
         if isinstance(keycode, tuple):
             keycode = keycode[1]
 
         thing = self.input.text
         
-        if (thing == "Enter new zipcode: "):
+        if (thing == "Enter input: "):
             thing = ""
-        
-        print(thing)
-        
+
+        # keycode translations
         if keycode == "spacebar":
             keycode = " "
         elif keycode == "backspace":
@@ -49,26 +45,32 @@ class VirtualKeyboard(GridLayout):
             keycode = ""
             if(self.return_func is not None):
                 self.return_func(self.input.text)
-            #self.username = f'{thing}'
-            #self.layout.remove_widget(keyboard)
-            #submit
-
+                
+        # update label
         self.input.text = f'{thing}{keycode}'
         pass
     
 
-class DigitKeyboard(VKeyboard):
-    def __init__(self, **kwargs):
+class DigitKeyboard(BoxLayout):
+    def __init__(self, return_func=None, **kwargs):
         super().__init__(**kwargs)
-    def key_up(self):
-        pass
-
-class VirtualKeyboardLayout(GridLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.rows = 1
-        self.virtual_keyboard = VirtualKeyboard()
-        self.add_widget(self.virtual_keyboard)
+        self.return_func = return_func
+        self.input = Label(text = "")
+        self.add_widget(self.input)
         
-
-    pass
+        for i in range(10):
+            digit = Button(text=str(i), size_hint=(0.2,0.2))
+            digit.bind(on_release=lambda button: self.appendInput(button.text))
+            self.add_widget(digit)
+        
+        self.enter = Button(text="Enter", size_hint=(0.2,0.2))
+        self.enter.bind(on_press = self.exitPopup)
+        self.add_widget(self.enter)
+        
+    def appendInput(self, text):
+        self.input.text = self.input.text + str(text)
+        pass
+    
+    def exitPopup(self, dt):
+        if(self.return_func is not None):
+                self.return_func(self.input.text)
