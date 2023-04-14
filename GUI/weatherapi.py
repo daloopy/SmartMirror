aa2052a216112b99dbe303a742e4ae8857d9231a
Dalilah Vaquera <70181295+daloopy@users.noreply.github.com>
2023-04-14 17:51:57 -0400

    Update weatherapi.py
    get user zipcode

--- GUI/weatherapi.py
+++ GUI/weatherapi.py
@@ -1,8 +1,12 @@
 import os
 import requests
 import json
+from user import *
 
 def getWeather(zipcode="47906"):
+    user = User()
+    zipcode = user.get_user_zipcode()
+    
     token= "b2d5146e59af44d2b4d153350231402" #os.getenv("WEATHERAPI_TOKEN")
     url = "http://api.weatherapi.com/v1/current.json?key={}&q={}".format(token, zipcode)
     response = requests.get(url)
@@ -12,5 +16,6 @@ def getWeather(zipcode="47906"):
     condition = data["current"]["condition"]["text"]
     return town, temp_f, condition
 
-town, temp, condition = getWeather("47906")
+#town, temp, condition = getWeather("47906")
+
 
