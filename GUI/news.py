94346ae61884d089eb9f762e288d84557a1872e5
WahabAkanbi <77656620+WahabAkanbi@users.noreply.github.com>
2023-04-26 22:02:42 -0400

    Update news.py

--- GUI/news.py
+++ GUI/news.py
@@ -1,21 +1,32 @@
 import requests
 
-# set the API endpoint and parameters
-url = 'https://newsapi.org/v2/top-headlines'
-params = {
-    'country': 'us',  # set the country to get headlines from
-    'apiKey': '023225675d1e49c59a6e149eb70f62cc'  # replace with your actual API key
-}
+def newsInit():
+    # set the API endpoint and parameters
+    url = 'https://newsapi.org/v2/top-headlines'
+    params = {
+        'country': 'us',  # set the country to get headlines from
+        'apiKey': '023225675d1e49c59a6e149eb70f62cc'  # replace with your actual API key
+    }
 
-# make the HTTP GET request to the API
-response = requests.get(url, params=params)
+    # make the HTTP GET request to the API
+    response = requests.get(url, params=params)
+    return response
 
-# parse the JSON response
-data = response.json()
+def getNews(response):
+    # parse the JSON response
+    data = response.json()
+    articles_str = ''
+    # display the headlines and news stories
+    for index, article in enumerate (data['articles']):
+        if index < 5:
+            articles_str += article['title'] + '\n' + article['description'] + '\n\n'
+        else:
+            break
+    return articles_str
 
-# display the headlines and news stories
-for article in data['articles']:
-    print(article['title'])
-    print(article['description'])
-    print(article['url'])
-    print()
+def news():
+    response = newsInit()
+    articles = getNews(response)
+    print(articles)
+
+news()
